cfdb27a3499d3b3fb5dc01d5a5ac4373f0720289
Martijn Beenker <martijn.beenker@gmail.com>
2018-10-14 11:08:38 +0200

    Opstellen van metadata over alle coordinaten in het spel

--- BattleShip/game/LoadConfig.sql
+++ BattleShip/game/LoadConfig.sql
@@ -1,6 +1,37 @@
-﻿CREATE PROCEDURE [dbo].[LoadConfig]
-	@param1 int = 0,
-	@param2 int
+﻿CREATE PROCEDURE [game].[LoadConfig]
 AS
-	SELECT @param1, @param2
-RETURN 0
+BEGIN
+	-- Empty the table
+	TRUNCATE TABLE [game].[Config];
+
+	-- Insert base values 
+	INSERT INTO game.Config (
+		[coordinate]             
+	)
+	SELECT col1
+	FROM player.Sea
+	UNION ALL
+	SELECT col2
+	FROM player.Sea
+	UNION ALL
+	SELECT col3
+	FROM player.Sea
+	UNION ALL
+	SELECT col4
+	FROM player.Sea
+	UNION ALL
+	SELECT col5
+	FROM player.Sea;
+
+	-- Fill in the metadata
+	UPDATE tgt
+	SET is_left_upper_corner = CASE WHEN coordinate = 41 THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END
+		, is_right_upper_corner = CASE WHEN coordinate = 45 THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END
+		, is_left_lower_corner = CASE WHEN coordinate = 1 THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END
+		, is_right_lower_corner = CASE WHEN coordinate = 5 THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END
+		, is_left_boundary = CASE WHEN RIGHT(coordinate, 1) = 1 THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END
+		, is_right_boundary = CASE WHEN RIGHT(coordinate, 1) = 5 THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END
+		, is_top_boundary = CASE WHEN LEFT(coordinate, 1) = 4 THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END
+		, is_bottom_boundary = CASE WHEN LEN(coordinate) = 1 THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END
+	FROM game.Config AS tgt;
+END
--- BattleShip/game/Tables/config.sql
+++ BattleShip/game/Tables/config.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [game].[config] (
-	[coordinate]            NVARCHAR(10) NOT NULL,
-    [is_left_upper_corner]  BIT NOT NULL,
-    [is_right_upper_corner] BIT NOT NULL,
-    [is_left_lower_corner]  BIT NOT NULL,
-    [is_right_lower_corner] BIT NOT NULL,
-    [is_left_boundary]      BIT NOT NULL,
-    [is_right_boundary]     BIT NOT NULL,
-    [is_top_boundary]       BIT NOT NULL,
-    [is_bottom_boundary]    BIT NOT NULL
+	[coordinate]            INT NOT NULL,
+    [is_left_upper_corner]  BIT		NULL,
+    [is_right_upper_corner] BIT		NULL,
+    [is_left_lower_corner]  BIT		NULL,
+    [is_right_lower_corner] BIT		NULL,
+    [is_left_boundary]      BIT		NULL,
+    [is_right_boundary]     BIT		NULL,
+    [is_top_boundary]       BIT		NULL,
+    [is_bottom_boundary]    BIT		NULL
 );
 
