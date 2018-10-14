b42b9ef1c9ecbe1e8d377d402c09a9ebea5c7913
Martijn Beenker <martijn.beenker@gmail.com>
2018-10-14 17:15:25 +0200

    Toevoegen van een zee-overzicht bij het eindigen van de game

--- BattleShip/game/PrintOutcome.sql
+++ BattleShip/game/PrintOutcome.sql
@@ -4,7 +4,7 @@ AS
 BEGIN
 	IF @outcome = 'draw'
 	BEGIN
-		EXEC PrintLogo;
+		EXEC game.PrintLogo;
 		PRINT '
 	 _____ _____ _____    ___   ____________  ___  _    _ 
 	|_   _|_   _/  ___|  / _ \  |  _  \ ___ \/ _ \| |  | |
@@ -12,13 +12,14 @@ BEGIN
 	  | |   | |  `--. \ |  _  | | | | |    /|  _  | |/\| |
 	 _| |_  | | /\__/ / | | | | | |/ /| |\ \| | | \  /\  /
 	 \___/  \_/ \____/  \_| |_/ |___/ \_| \_\_| |_/\/  \/ 
-';                                                                                                         
+';    
+		EXEC game.PrintSea;
 
 	END
 
 	IF @outcome = 'win'
 	BEGIN
-		EXEC PrintLogo;
+		EXEC game.PrintLogo;
 		PRINT '			
 	__   _______ _   _   _    _ _____ _   _ 
 	\ \ / /  _  | | | | | |  | |_   _| \ | |
@@ -27,12 +28,13 @@ BEGIN
 	  | | \ \_/ / |_| | \  /\  /_| |_| |\  |
 	  \_/  \___/ \___/   \/  \/ \___/\_| \_/
 ';
+		EXEC game.PrintSea;
 
 	END
 
 	IF @outcome = 'lose'
 	BEGIN
-		EXEC PrintLogo;
+		EXEC game.PrintLogo;
 		PRINT '
 	__   _______ _   _   _     _____ _____ _____ 
 	\ \ / /  _  | | | | | |   |  _  /  ___|  ___|
@@ -41,6 +43,7 @@ BEGIN
 	  | | \ \_/ / |_| | | |___\ \_/ /\__/ / |___ 
 	  \_/  \___/ \___/  \_____/\___/\____/\____/ 
 '; 
+		EXEC game.PrintSea;
 
 	END
 END
