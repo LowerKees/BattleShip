3679b48e31b7afeff290e78146490da9a68f431b
Martijn Beenker <martijn.beenker@gmail.com>
2018-10-14 17:15:08 +0200

    Bugfix voor WHERE statement (typo)

--- BattleShip/game/EvaluateTheSeas.sql
+++ BattleShip/game/EvaluateTheSeas.sql
@@ -13,43 +13,46 @@ BEGIN
 		UNION ALL
 		SELECT col2
 		FROM player.Sea
-		WHERE col1 = 77
+		WHERE col2 = 77
 		UNION ALL
 		SELECT col3
 		FROM player.Sea
-		WHERE col1 = 77
+		WHERE col3 = 77
 		UNION ALL
 		SELECT col4
 		FROM player.Sea
-		WHERE col1 = 77
+		WHERE col4 = 77
 		UNION ALL
 		SELECT col5
 		FROM player.Sea
-		WHERE col1 = 77) AS src;
+		WHERE col5 = 77) AS src;
 
 	SELECT @number_of_opponent_boats = COUNT(*) FROM (
 		SELECT col1
 		FROM opponent.Sea
-		WHERE col1 = 77
+		WHERE col1 = 66
 		UNION ALL
 		SELECT col2
 		FROM opponent.Sea
-		WHERE col1 = 77
+		WHERE col2 = 66
 		UNION ALL
 		SELECT col3
 		FROM opponent.Sea
-		WHERE col1 = 77
+		WHERE col3 = 66
 		UNION ALL
 		SELECT col4
 		FROM opponent.Sea
-		WHERE col1 = 77
+		WHERE col4 = 66
 		UNION ALL
 		SELECT col5
 		FROM opponent.Sea
-		WHERE col1 = 77) AS src;
+		WHERE col5 = 66) AS src;
 
-	IF @number_of_player_boats > 0 AND @number_of_opponent_boats > 0 SET @outcome = 'ingame'; RETURN;
-	IF @number_of_player_boats = 0 AND @number_of_opponent_boats = 0 SET @outcome = 'draw'; RETURN;
-	IF @number_of_player_boats > 0 AND @number_of_opponent_boats = 0 SET @outcome = 'win'; RETURN;
-	IF @number_of_player_boats = 0 AND @number_of_opponent_boats > 0 SET @outcome = 'lose'; RETURN;
+	PRINT CAST(@number_of_player_boats AS NVARCHAR(10));
+	PRINT CAST(@number_of_opponent_boats AS NVARCHAR(10));
+
+	IF @number_of_player_boats > 0 AND @number_of_opponent_boats > 0 BEGIN SET @outcome = 'ingame'; RETURN; END
+	IF @number_of_player_boats = 0 AND @number_of_opponent_boats = 0 BEGIN SET @outcome = 'draw'; RETURN; END
+	IF @number_of_player_boats > 0 AND @number_of_opponent_boats = 0 BEGIN SET @outcome = 'win'; RETURN; END
+	IF @number_of_player_boats = 0 AND @number_of_opponent_boats > 0 BEGIN SET @outcome = 'lose'; RETURN; END
 END
